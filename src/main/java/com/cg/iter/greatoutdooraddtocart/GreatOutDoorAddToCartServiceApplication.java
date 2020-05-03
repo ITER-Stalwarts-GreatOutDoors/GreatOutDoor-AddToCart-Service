2918946d25c690112881a22583f06f5ca62169be
sachiket <sachiketbehera@gmail.com>
2020-05-03 11:53:33 +0530

    first commit

---
+++ src/main/java/com/cg/iter/greatoutdooraddtocart/GreatOutDoorAddToCartServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cg.iter.greatoutdooraddtocart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreatOutDoorAddToCartServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreatOutDoorAddToCartServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cg/iter/greatoutdooraddtocart/GreatOutDoorAddToCartServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.iter.greatoutdooraddtocart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreatOutDoorAddToCartServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
